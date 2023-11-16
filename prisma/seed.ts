c5468e2a41c37c96992f2e981d00f66eae073dab
Abhi6722 <abhinav6722cool@gmail.com>
2023-11-16 23:05:57 +0530

    DB Change

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,39 +3,31 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user1 = await prisma.user.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
+
+  await prisma.user.create({
+    data: {
       username: "Isha",
       email: "isha@gmail.com",
       phone: "9898786787",
     },
-  });
+  })
 
-  const user2 = await prisma.user.upsert({
-    where: { id: 2 },
-    update: {},
-    create: {
+  await prisma.user.create({
+    data: {
       username: "John",
       email: "john@gmail.com",
       phone: "1234567890",
     },
-  });
+  })
 
-  const user3 = await prisma.user.upsert({
-    where: { id: 3 },
-    update: {},
-    create: {
+  await prisma.user.create({
+    data: {
       username: "Emma",
       email: "emma@gmail.com",
       phone: "9876543210",
     },
-  });
-
-  console.log({ user1, user2, user3 });
+  })
 }
-
 
 main()
   .then(() => prisma.$disconnect())
@@ -43,4 +35,4 @@ main()
     console.error(e);
     await prisma.$disconnect();
     process.exit();
-  });
+});
